df771655094fcf4b130d422269d6ca75b87fd234
Michael Miller <40524613+mikemillering@users.noreply.github.com>
2024-02-08 16:37:27 -0700

    udated UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,15 @@ function App() {
   const [display, setDisplay] = useState(<></>);
   const [updateDisplay, setUpdateDisplay] = useState(true);
   const [meetingDisplay, setMeetingDisplay] = useState(<></>);
-  const [displayToggle, setDisplayToggle] = useState(true);
+  const [startToggle, setStartToggle] = useState(false);
+  const [listToggle, setListToggle] = useState(true);
 
   useEffect(() => {
     setDisplay(
       <div>
         {list.map((item, key) => (
-          <div className="col" key={key}>
-            <div className="card pt-1 pb-0 m-0">
+          <div className="row" key={key}>
+            <div className="col-8 pt-1 pb-0 m-0">
               <p className="m-0" style={{ fontSize: "15px" }}>
                 {item[0]}
               </p>
@@ -24,26 +25,36 @@ function App() {
                 {" Duration: " + item[1] + " seconds"}
               </p>
             </div>
-            <button
-              className="btn btn-sm btn-outline-secondary p-1 mt-1"
-              onClick={() => {
-                removeItem(key);
-              }}
-            >
-              remove
-            </button>
+            <div className="col-4">
+              <button
+                className="btn btn-sm btn-outline-secondary p-1 mt-1"
+                onClick={() => {
+                  removeItem(key);
+                }}
+              >
+                remove
+              </button>
+            </div>
           </div>
         ))}
       </div>
     );
   }, [updateDisplay, list]);
 
+  useEffect(() => {
+    if (listToggle === true) {
+      setStartToggle(false);
+    } else {
+      setStartToggle(true);
+    }
+  }, [listToggle]);
+
   function addItem() {
     if (itemInput !== "" && durationInput !== "") {
       setList([...list, [itemInput, Number(durationInput)]]);
       setItemInput("");
       setDurationInput("");
-      setDisplayToggle(true);
+      setListToggle(true);
     }
   }
 
@@ -55,18 +66,23 @@ function App() {
   }
 
   function startMeeting() {
-    const meetingItems = list.map((item, key) => (
-      <div className="card pt-1 pb-0 m-0" key={key}>
-        <p className="m-0" style={{ fontSize: "15px" }}>
-          {item[0]}
-        </p>
-        <p className="m-0" style={{ fontSize: "10px" }}>
-          {" Duration: " + item[1] + " seconds"}
-        </p>
-      </div>
-    ));
-    setMeetingDisplay(meetingItems);
-    setDisplayToggle(false);
+    if (listToggle === true) {
+      const meetingItems = list.map((item, key) => (
+        <div className="pt-1 pb-0 m-0" key={key}>
+          <p className="m-0" style={{ fontSize: "15px" }}>
+            {item[0]}
+          </p>
+          <p className="m-0" style={{ fontSize: "10px" }}>
+            {" Duration: " + item[1] + " seconds"}
+          </p>
+        </div>
+      ));
+      setMeetingDisplay(meetingItems);
+      setListToggle(!listToggle);
+    } else {
+      setMeetingDisplay("");
+      setListToggle(!listToggle);
+    }
   }
 
   function handleItemInput(e) {
@@ -81,35 +97,46 @@ function App() {
   }
 
   return (
-    <>
-      <div className="card p-2 m-2">
-        <input
-          className="form-control mb-1"
-          type="text"
-          value={itemInput}
-          placeholder="Item"
-          onChange={handleItemInput}
-        />
-        <input
-          className="form-control mb-1"
-          type="text"
-          value={durationInput}
-          placeholder="Duration"
-          onChange={handleDurationInput}
-        />
-        <button className="btn btn-outline-secondary" onClick={addItem}>
-          Add
-        </button>
-      </div>
-      <div className="card p-2 m-2">{displayToggle ? display : null}</div>
-      <div className="card p-2 m-2">
-        <button className="btn btn-outline-secondary" onClick={startMeeting}>
-          Start Meeting
+    <div className="container">
+      {listToggle ? (
+        <div className="card p-2 m-2">
+          <input
+            className="form-control mb-1"
+            type="text"
+            value={itemInput}
+            placeholder="Item"
+            onChange={handleItemInput}
+          />
+          <input
+            className="form-control mb-1"
+            type="text"
+            value={durationInput}
+            placeholder="Duration"
+            onChange={handleDurationInput}
+          />
+          <button className="btn btn-outline-secondary" onClick={addItem}>
+            Add
+          </button>
+        </div>
+      ) : null}
+      <div className=" p-2 m-2">{listToggle ? display : null}</div>
+      <div className="p-2 m-2">
+        <button
+          className="btn btn-outline-secondary start-button mb-2"
+          onClick={startMeeting}
+        >
+          {startToggle ? "End Meeting" : "Start Meeting"}
         </button>
         {meetingDisplay}
       </div>
-    </>
+    </div>
   );
 }
 
 export default App;
+
+{
+  /*
+
+*/
+}
